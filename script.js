ac97f7e32954a44494e42b5a440ebbc7b08020d4
anna-n-seth <87625737+anna-n-seth@users.noreply.github.com>
2025-07-11 19:24:28 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.ekidz-slider', {
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
